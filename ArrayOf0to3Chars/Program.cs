5cce5ce0c12eb64becb15b412a24f0a10a0e0472
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-06-30 23:22:40 +0300

    Modified ArrayWith0to3Chars method to form a new array with only needed length

--- ArrayOf0to3Chars/Program.cs
+++ ArrayOf0to3Chars/Program.cs
@@ -12,11 +12,15 @@ string[] InputStringArray(int length)
     }
     return tempArray;
 }
-string[] FillArrayWith0to3CharsElements(string[] initialArray)
+string[] ArrayWith0to3Chars(string[] initialArray)
 {
     int length = initialArray.Length;
-    string[] tempArray = new string[length];
     int j = 0;
+    for (int i = 0; i < length; i++)   
+        if (initialArray[i].Length <= 3) // Проверка сколько элементов начального массива содержат <= 3 символа...
+            j++;
+    string[] tempArray = new string[j]; // ...чтобы создать новый массив только с необходимым кол-вом элементов.
+    j = 0;
     for (int i = 0; i < length; i++)
     {
         if (initialArray[i].Length <= 3)
@@ -35,3 +39,15 @@ string PrintArray(string[] array)
         temp = temp + $"{array[i]} ";
     return temp;
 }
+Console.Clear();
+Console.Write("Введите длину массива строк: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+string[] initialArray = InputStringArray(length);
+string[] finalArray = ArrayWith0to3Chars(initialArray);
+Console.WriteLine();
+Console.WriteLine("Введенный массив:");
+Console.WriteLine(PrintArray(initialArray));
+Console.WriteLine();
+Console.WriteLine("Сформированный массив из строк начального массива, длина которых меньше либо равна 3 символа:");
+Console.WriteLine(PrintArray(finalArray));
+Console.WriteLine();
